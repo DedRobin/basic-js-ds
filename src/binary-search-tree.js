8e87b43d2ba7fcea97ed93364e32c6f0ab88b375
Pavel Rabekin <pavelrabekin@gmail.com>
2024-12-16 16:08:51 +0300

    feat: implement binary search tree (methods add, has, find)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,55 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this._root = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root ? this._root : null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const node = new Node(data);
+
+    function addNode(root, current) {
+      if (current.data >= root.data) {
+        if (!root.right) root.right = current;
+        else addNode(root.right, current);
+      } else {
+        if (!root.left) root.left = current;
+        else addNode(root.left, current);
+      }
+    }
+
+    if (!this._root) this._root = node;
+    else addNode(this._root, node);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let node = this._root;
+    while (node) {
+      if (data === node.data) return true;
+      else if (data < node.data) node = node.left;
+      else if (data > node.data) node = node.right;
+    }
+    return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let node = this._root;
+    while (node) {
+      if (data === node.data) return node;
+      else if (data < node.data) node = node.left;
+      else if (data > node.data) node = node.right;
+    }
+    return node;
   }
 
   remove(/* data */) {
@@ -45,5 +69,23 @@ class BinarySearchTree {
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
+
+const binaryTree = new BinarySearchTree();
+console.log(binaryTree.root());
+binaryTree.add(2);
+binaryTree.add(3);
+binaryTree.add(4);
+console.log(binaryTree.root().data === 2);
+binaryTree.add(60);
+binaryTree.add(70);
+binaryTree.add(65);
+console.log(binaryTree.has(70));
+console.log(binaryTree.has(20));
+console.log(binaryTree.has(9));
+console.log(binaryTree.has(60));
+console.log(binaryTree.has(65));
+console.log(binaryTree.has(10));
+console.log(binaryTree.find(10));
+debugger;
