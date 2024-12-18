02312aa92f16ef081317491627986601b0de5494
Pavel Rabekin <pavelrabekin@gmail.com>
2024-12-18 13:33:37 +0300

    fix: remove method 'remove()'

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -42,34 +42,49 @@ class BinarySearchTree {
     return false;
   }
 
-  find(data) {
+  find(data, returnParent) {
     let node = this._root;
-    while (node) {
-      if (data === node.data) return node;
-      else if (data < node.data) node = node.left;
-      else if (data > node.data) node = node.right;
-    }
-    return node;
-  }
-
-  remove(data) {
-    let node = this.find(data);
     let parent = null;
     while (node) {
-      if (node.right) {
-        node.data = node.right.data;
+      if (data === node.data) return returnParent ? { parent, node } : node;
+      else if (data < node.data) {
+        parent = node;
+        node = node.left;
+      } else if (data > node.data) {
         parent = node;
         node = node.right;
-      } else if (node.left) {
-        node.data = node.left.data;
-      } else {
-        debugger;
-        if (parent.right) parent.right = null;
-        else if (parent.left) parent.left = null;
-        node = null;
       }
     }
+    return returnParent ? { parent, node } : node;
   }
+
+  remove(/*data*/) {
+    throw new NotImplementedError('Not implemented');
+  }
+  // remove(data) {
+  //   let { parent, node } = find(data, true);
+  //   // debugger
+  //   // if (node.data === 7) {
+  //   //   debugger;
+  //   // }
+  //   // while (node) {
+  //   //   if (node.right) {
+  //   //     node.data = node.right.data;
+  //   //     parent = node;
+  //   //     node = node.right;
+  //   //   } else if (node.left) {
+  //   //     parent.left.right = node.left;
+  //   //     node.left = null;
+  //   //     // node.data = node.left.data;
+  //   //     // parent = node;
+  //   //     // node = node.left;
+  //   //   } else {
+  //   //     if (parent && parent.right) parent.right = null;
+  //   //     else if (parent && parent.left) parent.left = null;
+  //   //     node = null;
+  //   //   }
+  //   // }
+  // }
 
   min() {
     throw new NotImplementedError('Not implemented');
@@ -88,7 +103,8 @@ module.exports = {
 
 const binaryTree = new BinarySearchTree();
 
-const numbers = [10, 6, 15, 4, 7, 14, 17, 16, 18];
-numbers.forEach((num) => binaryTree.add(num));
-binaryTree.remove(15);
-debugger;
+// const numbers = [10, 6, 15, 4, 7, 14, 17, 16, 18];
+// numbers.forEach((num) => binaryTree.add(num));
+// numbers.forEach((num) => binaryTree.remove(num));
+// // binaryTree.remove(10);
+// debugger;
